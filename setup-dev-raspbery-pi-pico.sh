c91fc5c8d3ccba59648151f68d12e076a6f14d5b
kakt <kakt@example.com>
2024-04-29 12:02:21 +0900

    Add  setup for raspbery pi pico.

---
+++ setup-dev-raspbery-pi-pico.sh
@@ -0,0 +1,49 @@
+
+# https://www.raspberrypi.com/documentation/microcontrollers/raspberry-pi-pico.html
+
+# Install
+cd ~/
+mkdir pico
+cd pico
+git clone -b master https://github.com/raspberrypi/pico-sdk.git
+cd pico-sdk
+git submodule update --init
+cd ..
+git clone -b master https://github.com/raspberrypi/pico-examples.git
+sudo apt update
+sudo apt -y install cmake gcc-arm-none-eabi build-essential 
+
+# PATH
+echo "export PICO_SDK_PATH=${HOME}/pico/pico-sdk" >> ~/.bashrc
+
+# Setup vscode
+sudo apt -y install code
+code --install-extension marus25.cortex-debug
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cpptools
+cd ~/pico/pico-examples
+mkdir .vscode
+cp ide/vscode/launch-raspberrypi-swd.json .vscode/launch.json
+cp ide/vscode/settings.json .vscode/settings.json
+
+# setup debbung
+cd ~/pico/pico-examples
+rm -rf build
+mkdir build
+cd build
+export PICO_SDK_PATH=../../pico-sdk
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+cd hello_world/serial
+make -j4
+
+sudo apt -y install gdb-multiarch
+
+# Install https://github.com/dorssel/usbipd-win/releases/tag/v4.2.0 in Windows
+
+sudo apt update
+sudo apt install binutils-multiarch
+cd /usr/bin
+ln -s /usr/bin/objdump objdump-multiarch
+ln -s /usr/bin/nm nm-multiarch 
+
+
--- setup-github.sh
+++ setup-github.sh
@@ -1,4 +1,5 @@
-sudo apt install gh
+sudo apt update
+sudo apt -y install gh
 gh auth login
 git config --global user.name kakt
 git config --global user.email kakt@example.com
